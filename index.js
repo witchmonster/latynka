dc35b10050cf3baef4c5395fb27ac2659267e0d3
Julia Kram <yuliya.kramarenko@gmail.com>
2023-01-22 22:17:52 +0000

    add browserify and tests

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { cyrToLat } from "./main/js/converters/cyrToLat.js";
 import { latToCyr } from "./main/js/converters/latToCyr.js";
+import { runTests } from "./test/js/test.js";
 
 const klatinoid = {
     cyrToLat,
-    latToCyr
+    latToCyr,
+    runTests
 }
 
 export { klatinoid }
